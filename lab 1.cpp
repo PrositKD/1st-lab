f424f6238dc515f94e39acee6862aec9320c43b4
Prosit Kumar Das <109513705+PrositKD@users.noreply.github.com>
2022-07-25 12:31:53 +0600

    Update lab 1.cpp

--- lab 1.cpp
+++ lab 1.cpp
@@ -1,6 +1,25 @@
 #include<iostream>
 using namespace std;
-int main()
-{
-    cout<<"Hello world"<<endl;
+
+int main() {
+    string n1;
+    string n2;
+    cout << "enter n1"<<endl;
+    cin>>n1;
+    cout << "enter n2"<<endl;
+    cin>>n2;
+   string str_inp1(n1);
+   string str_inp2(n2);
+
+cout<<"String 1:"<<str_inp1<<endl;
+cout<<"String 2:"<<str_inp2<<endl;
+int res = str_inp1.compare(str_inp2);
+
+    if (res == 0)
+        cout << "\nBoth the input strings are equal." << endl;
+    else if(res < 0)
+       
+        cout << "String 1 is smaller as compared to String 2\n.";
+        else
+        cout<<"String 1 is greater as compared to String 2\n.";
 }
